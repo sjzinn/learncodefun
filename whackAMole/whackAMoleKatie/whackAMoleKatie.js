2c81c43689dfebbb8d570f3d607f96b2fe18954e
Katie Barger <katieb@loopreturns.com>
2022-06-27 12:02:38 -0400

    Pushing up additional logic for game KB

--- whackAMole/whackAMoleKatie/whackAMoleKatie.js
+++ whackAMole/whackAMoleKatie/whackAMoleKatie.js
@@ -1,24 +1,24 @@
-const cursor = document.querySelector(".cursor");
-const holes = [...document.querySelectorAll(".hole")];
-const scoreEl = document.querySelector(".score span");
+const cursor = document.querySelector('.cursor');
+const holes = [...document.querySelectorAll('.hole')];
+const scoreEl = document.querySelector('.score span');
 let score = 0;
 
-const sound = new Audio("assets/smash.mp3");
+const sound = new Audio('assets/smash.mp3');
 
 function run() {
   const i = Math.floor(Math.random() * holes.length);
   const hole = holes[i];
   let timer = null;
 
-  const img = document.createElement("img");
-  img.classList.add("mole");
-  img.src = "assets/mole.png";
+  const img = document.createElement('img');
+  img.classList.add('mole');
+  img.src = 'assets/mole.png';
 
-  img.addEventListener("click", () => {
+  img.addEventListener('click', () => {
     score += 10;
     sound.play();
     scoreEl.textContent = score;
-    img.src = "assets/mole-whacked.png";
+    img.src = 'assets/mole-whacked.png';
     clearTimeout(timer);
     timer = setTimeout(() => {
       hole.removeChild(img);
@@ -35,15 +35,17 @@ function run() {
 }
 run();
 
-window.addEventListener("mousemove", (e) => {
-  cursor.style.top = e.pageY + "px";
-  cursor.style.left = e.pageX + "px";
+window.addEventListener('mousemove', (e) => {
+  cursor.style.top = e.pageY + 'px';
+  cursor.style.left = e.pageX + 'px';
 });
 
-window.addEventListener("mousedown", () => {
-  cursor.classList.add("active");
+window.addEventListener('mousedown', () => {
+  cursor.classList.add('active');
 });
 
-window.addEventListener("mouseup", () => {
-  cursor.classList.remove("active");
+window.addEventListener('mouseup', () => {
+  cursor.classList.remove('active');
 });
+
+yolo += 1;
