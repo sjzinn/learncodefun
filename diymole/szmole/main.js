f09c5bf44a96da45c18b970d155a05088566935b
Sarah Zinn <sarahzinn@loopreturns.com>
2022-06-27 11:23:33 -0400

    vue app working

---
+++ diymole/szmole/main.js
@@ -0,0 +1,63 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            holes: [
+                {
+                    id: 1,
+                    moleImage: './assets/mole.png',
+                    active: false
+                },
+                {
+                    id: 2,
+                    moleImage: './assets/mole.png',
+                    active: false
+                },
+                {
+                    id: 3,
+                    moleImage: './assets/mole.png',
+                    active: false
+                },
+                {
+                    id: 4,
+                    moleImage: './assets/mole.png',
+                    active: false
+                },
+                {
+                    id: 5,
+                    moleImage: './assets/mole.png',
+                    active: false
+                },
+                {
+                    id: 6,
+                    moleImage: './assets/mole.png',
+                    active: false
+                },
+                {
+                    id: 7,
+                    moleImage: './assets/mole.png',
+                    active: false
+                },
+                {
+                    id: 8,
+                    moleImage: './assets/mole.png',
+                    active: false
+                },
+                {
+                    id: 9,
+                    moleImage: './assets/mole.png',
+                    active: false
+                }
+            ],
+        }
+    },  
+    methods: {
+        randomize(holes) { 
+            const i = (Math.floor(Math.random() * this.holes.length));
+            console.log(this.holes.id);
+            if (this.holes.id === i) {
+                return active === true
+            }            
+        },
+
+    },
+})
